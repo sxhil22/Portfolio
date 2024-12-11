3cc5d722933a5912785323c9c763a053f182774d
sxhil22 <151609053+sxhil22@users.noreply.github.com>
2024-12-11 10:10:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling when navigating
+document.querySelectorAll('header nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
